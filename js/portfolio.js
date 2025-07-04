a42b69a74d794dcb8b433c12d6c02ad7de6fb7be
anchaewon <codnjsm@gmail.com>
2025-07-05 04:43:47 +0900

    250705 메인페이지 작업

--- js/portfolio.js
+++ js/portfolio.js
@@ -12,3 +12,53 @@ $(function () {
 
   $('#footer').load(base + 'html/footer.html');
 });
+
+// 원형 텍스트 
+$(document).ready(function () {
+  const text = "WEB PUBLISHER • FRONTEND • JAVASCRIPT • REACT • HTML • CSS • ";
+  const chars = text.split("");
+  const radius = 240;
+  const $container = $("#circularText");
+  const total = chars.length;
+
+  chars.forEach((char, i) => {
+    const angle = (360 / total) * i;
+    const $span = $("<span>").text(char);
+    $span.css({
+      transform: `rotate(${angle}deg) translate(${radius}px) rotate(${90}deg)`,
+    });
+    $container.append($span);
+  });
+});
+
+// 타이핑 효과
+$(function () {
+  const words = ["CHAE WON","WEB DEVELOPER"];
+  let currentWord = 0;
+  let currentChar = 0;
+  let isDeleting = false;
+  const $typing = $(".name .typing");
+  const typingSpeed = 150;
+  const pauseTime = 1200;
+
+  function typeEffect() {
+    const fullText = words[currentWord];
+    const visibleText = isDeleting
+      ? fullText.substring(0, currentChar--)
+      : fullText.substring(0, currentChar++);
+
+    $typing.text(visibleText);
+
+    if (!isDeleting && currentChar === fullText.length + 1) {
+      isDeleting = true;
+      setTimeout(typeEffect, pauseTime);
+    } else if (isDeleting && currentChar === 0) {
+      isDeleting = false;
+      currentWord = (currentWord + 1) % words.length;
+      setTimeout(typeEffect, 400);
+    } else {
+      setTimeout(typeEffect, isDeleting ? 50 : typingSpeed);
+    }
+  }
+  typeEffect();
+});
