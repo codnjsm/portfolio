14dc718ff95321aa2311f11ba5f68263056244fe
anchaewon <codnjsm@gmail.com>
2025-07-05 19:22:26 +0900

    250705 트랜지션 js 수정

--- js/transition.js
+++ js/transition.js
@@ -10,21 +10,38 @@ $(function () {
     });
   }
 
-  // 페이지 로드 후 등장 애니메이션
-  $(window).on('load', function () {
-    const $preloader = $('#preloader');
-    const $wrapper = $('.wrapper');
+  // // 페이지 로드 후 등장 애니메이션
+  // $(window).on('load', function () {
+  //   const $preloader = $('#preloader');
+  //   const $wrapper = $('.wrapper');
 
-    if ($preloader.length) {
-      setTimeout(function () {
-        $preloader.fadeOut(500, function () {
-          $wrapper.addClass('fade-in');
-        });
-      }, 1600);
-    } else {
+  //   if ($preloader.length) {
+  //     setTimeout(function () {
+  //       $preloader.fadeOut(500, function () {
+  //         $wrapper.addClass('fade-in');
+  //       });
+  //     }, 1600);
+  //   } else {
+  //     $wrapper.addClass('fade-in');
+  //   }
+  // });
+  // transition.js 수정
+$(window).on('load', function () {
+  const $wrapper = $('.wrapper');
+
+  // preloader 여부와 관계없이 무조건 fade-in
+  setTimeout(function () {
+    $('#preloader').fadeOut(500, function () {
       $wrapper.addClass('fade-in');
-    }
-  });
+    });
+  }, 1500);
+
+  // fallback: preloader 없어도 fade-in
+  if (!$('#preloader').length) {
+    $wrapper.addClass('fade-in');
+  }
+});
+
 
   // 페이지 전환 애니메이션
   $(document).on('click', 'a', function (e) {
