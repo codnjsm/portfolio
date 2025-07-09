ecde5f87b018b730107199299f9e349966b68ffc
anchaewon <cwahn@anchors-biz.com>
2025-07-09 13:40:12 +0900

    250709 트랜지션 js 수정

--- js/transition.js
+++ js/transition.js
@@ -14,6 +14,28 @@ $(function () {
       $wrapper.addClass('enter-sub');
     }
   }
+  
+ // 헤더/푸터 모두 로드 완료 후에 transition 적용
+  const isGithubPages = location.hostname.includes('github.io');
+  const base = isGithubPages ? '/portfolio/' : '/';
+ 
+  let loadCount = 0;
+ 
+  function onPartLoaded() {
+    loadCount++;
+    if (loadCount === 2) {
+      applyTransition();
+    }
+  }
+ 
+  $('#header').load(base + 'html/header.html', function () {
+    $('.logo').attr('href', base + 'index.html');
+    $('.link-works').attr('href', base + 'html/works.html');
+    $('.link-about').attr('href', base + 'html/aboutMe.html');
+    $('.link-contact').attr('href', base + 'html/contact.html');
+    onPartLoaded();
+  });
+ 
+  $('#footer').load(base + 'html/footer.html', onPartLoaded);
 
-  $(window).on('load', applyTransition);
 });
