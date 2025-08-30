50b5e7a704a8b007df2a8fd94580f4abdbc180f1
anchaewon <codnjsm@gmail.com>
2025-08-31 02:12:24 +0900

    works 작업

--- js/portfolio.js
+++ js/portfolio.js
@@ -65,4 +65,71 @@ $(document).ready(function () {
 
         typeEffect();
     });
+
+    // scroll lottie
+    let lottieVisible = true;
+    $(window).on('scroll', function () {
+        const scrollTop = $(window).scrollTop();
+        const $lottie = $('.intro .scroll-lottie-01');
+
+        if (scrollTop > 10 && lottieVisible) {
+            lottieVisible = false;
+            $lottie.stop().fadeOut(600);
+        } else if (scrollTop <= 10 && !lottieVisible) {
+            lottieVisible = true;
+            $lottie.stop().fadeIn(600);
+        }
+    });
+});
+
+// works swiper
+$(function () {
+    var SPEED = 600;
+
+    // 왼쪽: 이미지
+    var imgSwiper = new Swiper('.img-swiper', {
+        loop: true,
+        effect: 'cards',
+        grabCursor: true,
+        speed: SPEED,
+        pagination: {
+            el: '.img-swiper .swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.img-swiper .swiper-button-next',
+            prevEl: '.img-swiper .swiper-button-prev',
+        },
+    });
+
+    // 오른쪽: 설명 (이미지에만 조작, 설명은 따라오기만)
+    var descSwiper = new Swiper('.desc-swiper', {
+        loop: true,
+        effect: 'fade',
+        fadeEffect: { crossFade: true },
+        allowTouchMove: false,
+        speed: SPEED,
+    });
+
+    // 인덱스 동기화 (loop에서도 안정)
+    imgSwiper.on('slideChangeTransitionStart', function () {
+        descSwiper.slideToLoop(imgSwiper.realIndex, imgSwiper.params.speed, false);
+    });
+
+    // (권장) 슬라이드 개수/순서 확인
+    var imgCount = document.querySelectorAll(
+        '.img-swiper .swiper-wrapper .swiper-slide:not(.swiper-slide-duplicate)'
+    ).length;
+    var descCount = document.querySelectorAll(
+        '.desc-swiper .swiper-wrapper .swiper-slide:not(.swiper-slide-duplicate)'
+    ).length;
+    if (imgCount !== descCount) {
+        console.warn(
+            '[Works] 슬라이드 개수가 다릅니다. 동기화 정확도를 위해 개수/순서를 맞추세요. (img:',
+            imgCount,
+            ', desc:',
+            descCount,
+            ')'
+        );
+    }
 });
